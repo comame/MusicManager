676f6e5dae54a3ca4cd9ab510c200b2c49cc304d
comame <dev@comame.xyz>
2026-04-12 15:35:21 +0900

    テストプロジェクトを仮設

---
+++ Projects/Core/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace Core;
+
+public class Class1
+{
+    public static string GetMessage()
+    {
+        return "Hello, world!";
+    }
+}
---
+++ Projects/Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Core;
+
+namespace Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var msg = Class1.GetMessage();
+        Assert.Equal("Hello, world!!", msg);
+    }
+}
